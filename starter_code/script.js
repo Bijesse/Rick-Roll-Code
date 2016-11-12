908d356c21d987dc03557e4533ea2d9e95c253cf
Philip Makings <pmakings@salesforce.com>
2016-11-12 03:21:06 +0000

    Rickrolling

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,13 +1,38 @@
 //Task 1: Console the year in which the song "Beat it" was relased 
-
+//console.log(library.music.eighties.Michael_Jackson[1].year);
 
 //Task 2: Console the name of the song Lady Gaga released in 2006 
-
+//console.log(library.music.two_thousands.Lady_Gaga[0].song);
+//console.log("hello");
 
 
 $( document ).ready(function() {
 //Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
-    
+   //$("#output").html(library.music.twenty_tens.Adele[0].song);
 //Task 4: (project) Create a site that can display songs from an artist specified by a user search.
-
+$("#submitButton").click(function()
+{
+    var artistSelection = $("#dropDown").val();
+    console.log(artistSelection);
+    
+    if (artistSelection === "Michael_Jackson")
+    {
+        var jackoLen = library.music.eighties.Michael_Jackson.length;
+        console.log(jackoLen);
+        var i = 0;
+        for (; i < jackoLen; i ++)
+        {
+        //$("#outputForm").append(library.music.eighties.Michael_Jackson[0].song);
+        console.log(library.music.eighties.Michael_Jackson[i].song);
+        $("#outputForm").append('<br>' + library.music.eighties.Michael_Jackson[i].song + '</br>');
+        }
+            
+           // $("#outputForm").html(library.music.eighties.Michael_Jackson)
+           // );
+    }
+    
+    
+    //$("#outputForm").html(artistSelection);
+}
+);
 });
